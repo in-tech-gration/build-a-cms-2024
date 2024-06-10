62f0d9743c8abe36e4a351c42b98c06310ff0aa9
Kostas Minaidis <minaidisk@gmail.com>
2024-06-10 13:03:56 +0200

    Add basic routing: Home and Login

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-// function NAME() :<TypeScript type for return> {
-function test(): void {
-  console.log(process.env.DB);
-  console.log(process.env.PORT);
-  console.log("Server started running!");
-}
-test();
+// import * as http from "http";
+import http from "node:http"; // In production, switch to https
+import url from "node:url";
+import { IncomingMessage, ServerResponse } from "node:http";
+
+function handleRequest( req:IncomingMessage, res:ServerResponse ){
+  // Handle requests: handle the / (home) and the /admin (domain.com, domain.com/admin/)
+  if ( typeof req.url === "undefined" ){
+    return res.end("Something...");
+  }
+  const { pathname } = url.parse(req.url);
+  if ( pathname === "/" ){
+    return res.end("Home");
+  }
+  return res.end("Login");
+} 
+const server = http.createServer(handleRequest);
+
+server.listen(process.env.PORT, ()=>{
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+})
