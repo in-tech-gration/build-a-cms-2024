1dd99e131873c632238d9c2eb6090e0a9e0266b7
Mary Richelle <mary.richal44@gmail.com>
2024-06-19 10:30:51 +0300

    add <body>

--- frontend/head.ts
+++ frontend/head.ts
@@ -7,6 +7,7 @@
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>${title}</title>
         </head>
+        <body>
         <h1>${title}</h1>
        `
   );
